9eb55fda50093625ee2b3d5198355649ea57101f
kaileighrose <kaileighrose@gmail.com>
2016-11-13 23:03:26 -0500

    config basic stateprovider state, add pagination functionality to allresources view, add basic heading to board view

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
     .module('app', ['ui.router', 'ngSanitize'])
+    .config([
+      '$stateProvider',
+      '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+          .state('home', {
+            url: '/',
+            templateUrl: '/board.html',
+            controller: 'MainCtrl'
+          });
+
+      $urlRouterProvider.otherwise('home');
+    }])
