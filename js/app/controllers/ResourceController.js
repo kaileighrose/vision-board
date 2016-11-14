86644d7bda8be255c012bad2c6b8f7e8bddfc462
kaileighrose <kaileighrose@gmail.com>
2016-11-13 22:42:33 -0500

    add pagination functions to ResourceController

--- js/app/controllers/ResourceController.js
+++ js/app/controllers/ResourceController.js
@@ -1,5 +1,32 @@
-function ResourceController() {
+var RESOURCES_PER_PAGE = 20;
+function ResourceController(resources) {
+  var ctrl = this;
+  ctrl.data = [];
   
+  ctrl.page = 0;
+  ctrl.totalResources = resources.data.length;
+  ctrl.totalPages = Math.ceil(ctrl.totalResources / RESOURCES_PER_PAGE);
+
+  ctrl.paginateResources = function () {
+    ctrl.data = resources.data.slice(ctrl.page * RESOURCES_PER_PAGE, (ctrl.page + 1) * RESOURCES_PER_PAGE);
+    var info;
+    for (var i = 0; i < ctrl.posts.length; i++) {
+      info = TopService.getPost(ctrl.posts[i])
+      ctrl.detail.push(info.$$state);
+    }
+  };
+
+  ctrl.nextPage = function () {
+    ctrl.page++;
+    ctrl.paginateResources()
+  };
+
+  ctrl.previousPage = function () {
+    ctrl.page--;
+    ctrl.paginateResources();
+  };
+
+  ctrl.paginateResources();
 }
 
 angular
