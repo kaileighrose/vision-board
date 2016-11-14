095dc69cd5c3f208be8a1c99dbd7bbda9ee053cb
kaileighrose <kaileighrose@gmail.com>
2016-11-14 15:45:58 -0500

    add columns to create boards migration

--- db/migrate/20161114204309_create_boards.rb
+++ db/migrate/20161114204309_create_boards.rb
@@ -1,7 +1,8 @@
 class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
-
+      t.text :board
+      t.text :items
       t.timestamps null: false
     end
   end
