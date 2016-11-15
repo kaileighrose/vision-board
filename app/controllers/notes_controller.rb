1704d59189859528f50ea0b2662ff29c3df8d462
kaileighrose <kaileighrose@gmail.com>
2016-11-15 17:08:48 -0500

    add CRUD actions to rails notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,40 @@
 class NotesController < ApplicationController
 
+  def new
+    @note = Note.new
+    render json: @note 
+  end
+
+  def create
+    @note = Note.create(note_params)
+    if @note.save
+      render json: @note 
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+    render json: @note 
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+    if @note.save
+      render json: @note 
+    end
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render json: @note 
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+  end
+
   private
 
   def note_params
