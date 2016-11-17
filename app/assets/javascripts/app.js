ff87afe087356bbd92aa26e5300d5410bee9c762
kaileighrose <kaileighrose@gmail.com>
2016-11-16 20:44:38 -0500

    add state to controller, fix UserService file's language, fill in basics of UserService

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,26 @@
 angular
     .module('app', ['ui.router', 'ngSanitize'])
-    .config([
-      '$stateProvider',
-      '$urlRouterProvider',
-      function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-          .state('home', {
-            url: '/',
-            templateUrl: '/board.html',
-            controller: 'MainCtrl'
-          });
-
-      $urlRouterProvider.otherwise('home');
+            .state('home', {
+                url: '/',
+                templateUrl: '/views/resources/index.html',
+                controller: 'UserController as user',
+                resolve: {
+                  user: function ($routeParams, UserService) {
+                    return UserService.getUser($routeParams.id);
+                  }
+                }
+            })
+            .state('post', {
+                url: '/post/:id',
+                templateUrl: '/views/post.html',
+                controller: 'PostController as post',
+                resolve: {
+                    post: function ($stateParams, TopService) {
+                        return TopService.getPost($stateParams.id);
+                    }
+                }
+            });
+    $urlRouterProvider.otherwise('/top');
     }])
