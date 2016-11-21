6b2d4c9991b0b3ae99efd548052f174426753dd8
kaileighrose <kaileighrose@gmail.com>
2016-11-21 12:38:00 -0500

    add function to make resource form appear when clicked

--- app/assets/javascripts/controllers/ResourceController.js
+++ app/assets/javascripts/controllers/ResourceController.js
@@ -1,7 +1,14 @@
 function ResourceController(ResourceService) {
   var ctrl = this;
-  ctrl.types = ['link', 'idea', 'image link']
+  ctrl.types = ['link', 'idea', 'image link'];
+  ctrl.form = false;
   //need to add name column to table
+
+  ctrl.showForm = function () {
+    ctrl.form = true;
+    $scope.apply();
+  }
+
   ctrl.load = function () {
     ctrl.resources = ResourceService.getResources();
     return ctrl.resources;
