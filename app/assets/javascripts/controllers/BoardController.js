10a5ec7040085c2d988525d4233bf56c0c36651d
kaileighrose <kaileighrose@gmail.com>
2016-11-20 23:00:20 -0500

    build out basic functions of BoardController

--- app/assets/javascripts/controllers/BoardController.js
+++ app/assets/javascripts/controllers/BoardController.js
@@ -3,6 +3,23 @@ function BoardController(BoardService) {
   ctrl.data = [];
   ctrl.boards = {};
   // boardname => [resources]
+
+  ctrl.load = function () {
+    ctrl.boards = BoardService.getBoards();
+    return ctrl.boards;
+  }
+
+  ctrl.create = function (resource) {
+    var res = ResourceService.addResource(resource);
+    ctrl.resources.push(res);
+    return ctrl.resources;
+  }
+
+  ctrl.get = function (id) {
+    return ResourceService.getOneResource(id);
+  }
+
+  ctrl.load();
 }
 
 angular
