6efae2020fcf6dfb5b4ec78ed61914e3ade4e7a2
kaileighrose <kaileighrose@gmail.com>
2016-11-26 17:00:53 -0500

    add more validations to forms

--- app/assets/javascripts/directives/signForm.js
+++ app/assets/javascripts/directives/signForm.js
@@ -3,9 +3,9 @@ function signForm() {
         template: [
             '<div class="userForm">',
                 '<form name="form" ng-submit="user.signUser(user)">',
-                  '<input type="text" name="username" required="required" placeholder="Username" ng-model="user.username"></input>',
+                  '<input type="text" name="username" required="required" minlength="3" maxlength="20" placeholder="Username" ng-model="user.username"></input>',
                   '<br>',
-                  '<input type="text" name="password" required="required" placeholder="Password"  ng-model="user.password"></input>',
+                  '<input type="text" name="password" required="required" minlength="5" maxlength="10" placeholder="Password"  ng-model="user.password"></input>',
                   '<br>',
                   '<button type="submit">Sign up</button>',
                 '</form>',
