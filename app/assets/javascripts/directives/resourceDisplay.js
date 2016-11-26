ecdf8d8a1967880d05fe5b16642def464c8747bd
kaileighrose <kaileighrose@gmail.com>
2016-11-26 00:05:35 -0500

    add formatting to fix element and content  alignments

--- app/assets/javascripts/directives/resourceDisplay.js
+++ app/assets/javascripts/directives/resourceDisplay.js
@@ -2,7 +2,7 @@ function resourceDisplay() {
   return {
         template: [
           '<div ng-switch on="resource.kind">',
-            ' <div ng-switch-when="image link"><img src="{{resource.item}}">{{resource.name}}</img></div>',
+            ' <div ng-switch-when="image link"><h3>{{resource.name}}</h3><img src="{{resource.item}}"></img></div>',
             '<div ng-switch-when="link"><a href="resource.item">{{resource.name}}</a></div>',
             ' <div ng-switch-default><h3>Title: {{resource.name}}</h3><p>{{resource.item}}</p></div>',
           '</div>'
